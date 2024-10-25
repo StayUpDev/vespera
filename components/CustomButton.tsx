4869e7d1636ad0848409a0666b2d295db323fa47
vm <vm@debian12-2024.lancw>
2024-10-25 20:34:35 +0200

    Added CustonButton.tsx from button-feature to main

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,4 +1,10 @@
-import { ActivityIndicator, Text, TouchableOpacity } from "react-native";
+import React, { useRef, useState} from "react";
+import {
+  ActivityIndicator,
+  Text,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
 
 const CustomButton = ({
   title,
@@ -7,26 +13,70 @@ const CustomButton = ({
   textStyles,
   isLoading,
 }) => {
-  return (
-    <TouchableOpacity
-      onPress={handlePress}
-      activeOpacity={0.7}
-      className={`bg-[#7F6AF3] rounded-full  flex flex-row justify-center items-center  ${containerStyles} ${
-        isLoading ? "opacity-50" : ""
-      }`}
-      disabled={isLoading}
-    >
-      <Text className={`text-primary  text-lg ${textStyles}`}>{title}</Text>
+  const scaleValue = useRef(new Animated.Value(1)).current;
+  const [shadowStyle, setShadowStyle] = useState({
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  });
 
-      {isLoading && (
-        <ActivityIndicator
-          animating={isLoading}
-          color="#fff"
-          size="small"
-          className="ml-2"
-        />
-      )}
-    </TouchableOpacity>
+  const animateIn = () => {
+    Animated.spring(scaleValue, {
+      toValue: 0.99,
+      useNativeDriver: true,
+    }).start();
+
+    setShadowStyle({
+      shadowColor: "#333",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      shadowRadius: 2,
+    });
+  };
+
+  const animateOut = () => {
+    Animated.spring(scaleValue, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+
+    setShadowStyle({
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.3,
+      shadowRadius: 4,
+    });
+  };
+
+  return (
+      <TouchableOpacity
+        onPress={handlePress}
+        onPressIn={animateIn}
+        onPressOut={animateOut}
+        activeOpacity={0.7}
+        className={`bg-[#7F6AF3] rounded-full h-min w-max flex pt-2 pb-2 pt-3.5 pb-3.5  justify-center items-center ${containerStyles} ${
+          isLoading ? "opacity-50" : ""
+        }`}
+        style={[{ transform: [{ scale: scaleValue }] }, shadowStyle]}
+        disabled={isLoading}
+      >
+        <Text
+          className={`text-[#D8DFE9] text-lg font-pextralight text-2xl ${textStyles} ${
+            isLoading ? "hidden" : ""
+          }`}
+        >
+          {title}
+        </Text>
+
+        {isLoading && (
+          <ActivityIndicator
+            animating={isLoading}
+            color="#D8DFE9"
+            size="small"
+          />
+        )}
+      </TouchableOpacity>
   );
 };
 
